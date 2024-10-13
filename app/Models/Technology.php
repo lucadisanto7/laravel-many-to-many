d6d7a09bf073dc87c27693db4e733167712df7a0
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-13 22:56:29 +0200

    [UPD] Added functions

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Technology extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'slug'];
+    public static function generateSlug($title){
+        return Str::slug($title, '-');
+    }
+    public function projects(){
+        return $this->belongsToMany(Project::class);
+    }
 }
