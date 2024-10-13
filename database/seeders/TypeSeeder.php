10950f49f26451a208958cf2d6154d2ab423559f
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-13 22:42:30 +0200

    first commit

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $types = ['Frontend', 'Backend', 'Fullstack', 'Database', 'Design'];
+
+        foreach($types as $type){
+            $new_type = new Type();
+            $new_type->name = $type;
+            $new_type->slug = Type::generateSlug($type);
+            $new_type->save();
+        }
+    }
+}
